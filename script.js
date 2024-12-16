2501c1c5e0e735470b643b9e2c4b8a109db986bb
Ravi Choudhary <151042374+jatravi@users.noreply.github.com>
2024-12-16 20:13:45 +0530

    added animation

--- script.js
+++ script.js
@@ -6,7 +6,63 @@ toggleBtn.addEventListener("click", () => {
     navBar.querySelector(".nav-links").classList.toggle("show");
 });
 
+// For the popup animation till line 38
 
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".container-ai h1", scrollRevealOption);
+
+ScrollReveal().reveal(".header__container p", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__container label", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+// About Us Container
+ScrollReveal().reveal(".container", scrollRevealOption);
+
+ScrollReveal().reveal(".container .sub-title", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".container #card", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+// ScrollReveal().reveal(".about__container .btn", {
+//     ...scrollRevealOption,
+//     delay: 1500,
+// });
+
+// // Discover Container
+
+// ScrollReveal().reveal(".discover__card", {
+//     ...scrollRevealOption,
+//     interval: 500,
+// })
+
+// ScrollReveal().reveal(".discover__card__content", {
+//     ...scrollRevealOption,
+//     interval: 500,
+//     delay: 200,
+// })
+
+// // Blogs Container
+// ScrollReveal().reveal(".blogs__card", {
+//     duration: 1000,
+//     interval: 400,
+// });
 
 // Sarthi BOT
 
